225fe861efb570924016e92a71d8fecd8d8f812b
deven065 <devenrikame55@gmail.com>
2025-04-11 01:48:33 +0530

    successfully wrote signup and signin Schemas in backend/routes/user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,37 +7,79 @@ const router = express.Router();
 // signup and signin routes
 
 const signupSchema = zod.object({
-    username : zod.string(),
+    username : zod.string().email(),
     password : zod.string(),
     firstname : zod.string(),
     lastName : zod.string()
 })
-router.post("/signup", async(req, res) => {
-    const body = req.body;
-    const {success} = signupSchema.safeParse(req.body);
+
+router.post('/signup', async (req, res) => {
+    const { success } = signupBody.safeParse(req.body)
     if (!success) {
-        return res.json({
-            message : "Email already taken / Incorrect inputs"
+        return res.status(411).json({
+            message : "Email already taken/ Incorrect inputs"
         })
     }
-
-    const user = User.findOne({
-        username : body.username
+    
+    const existingUser = await User.findOne({
+        username : req.body.username
     })
 
-    if(user.id) {
-        return res.json({
+    if (existingUser) {
+        return res.status(411).json({
+            message : "Email already taken/ Incorrect inputs"
+        })
+    }
+
+    const user = await User.create({
+        username : req.body.username,
+        password : req.body.password,
+        firstName : req.body.firstName,
+        lastName : req.body.lastName
+    })
+    const userId = user._id;
+
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
+
+    res.json({
+        message : "User created successfully",
+        token : token,
+    })
+})
+
+const signinSchema = zod.object({
+    username : zod.string().email(),
+    password : zod.string()
+})
+
+router.post("/signin", async (req, res) => {
+    const { success } = signinSchema.safeParse(req.body)
+    if (!success) {
+        return res.status(411).json({
             message : "Email already taken / Incorrect inputs"
         })
     }
 
-    const dbUser = await User.create(body);
-    const token = jwt.sign({
-        userId : dbUser._id
-    },JWT_SECRET)
-    res.json({
-        message : "User created successfully",
-        token : token
+    const user = await User.findOne({
+        username : req.body.username,
+        password : req.body.password
+    });
+
+    if (user) {
+        const token = jwt.sign({
+            userId : user._id
+        }, JWT_SECRET);
+
+        res.json({
+            token : token
+        })
+        return;
+    }
+
+    res.status(411).json({
+        mesasge : "Error while logging in"
     })
 })
 
