8558ca7bb2779985c9e8b88c860274b4a1a78bb8
Deven065 <devenrikame55@gmail.com>
2025-04-23 02:37:22 +0530

    Frontend re-worked

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    sourcemap: false, // turn off sourcemaps for production build
-  }
+  plugins: [react(), tailwindcss()]
 })
